0d4598b5a316134933f4cdd2b403c7cb998b162e
Bhushan Kiswe <surajkiswe777@gmail.com>
2025-07-27 22:25:01 +0530

    push todays everything

--- Services/GovernmentService/obj/Release/net8.0/GovernmentService.AssemblyInfo.cs
+++ Services/GovernmentService/obj/Release/net8.0/GovernmentService.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GovernmentService")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6e7a8e1ca2555c0b2e0f7766dd412c1795b0c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GovernmentService")]
